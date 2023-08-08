79829d38a6e584578919756d84d91ee96acdc928
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-08 01:35:06 -0400

    u

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
                 </span>
             </div>
         </div>
-        <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0" style="background-color: #161616;">
+        <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-4 z-0" style="background-color: #161616;">
             <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
                 <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
             </div>
