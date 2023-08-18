dc73cbb6163fd19d8efad64503bad6a8216d18d5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-17 22:19:48 -0400

    u

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
