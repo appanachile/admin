a2e26241b00543c6b8a3f1b3fc033aaaf58d5272
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-17 22:11:46 -0400

    u

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -10,9 +10,7 @@ use Laravel\Socialite\Facades\Socialite;
 class GoogleController extends Controller
 {
     public function login(){
-
         return Socialite::driver('google')->redirect();
-        
     }
 
     public function callback(){
