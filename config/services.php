329a4c54c33e965443c209aa003d704dc71a5f11
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-17 22:13:51 -0400

    u

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_OAUTH_ID'),
         'client_secret' => env('GOOGLE_OAUTH_KEY'),
-        'redirect' => '/google-callback',
+        'redirect' => 'https://admin.appana.cl/google-callback',
     ],
 
 ];
