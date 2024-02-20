c03d6a9fc4a8a6d6d9d2a2d7a6213542c797197c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-20 11:52:32 -0300

    u

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,6 +41,7 @@
                   <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
                    Actualizar
                   </button>
+                  <p class="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">Saltar</p>
                {!! Form::close() !!}
                </div>
             </div>
