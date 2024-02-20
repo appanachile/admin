f768fdac294c306cac28c180cdc2e653b2c17914
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-20 12:17:22 -0300

    u

---
+++ database/migrations/2024_02_20_120654_create_empresas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+            ->constrained()
+            ->onDelete('cascade');
+
+            $table->string('name');
+
+            $table->string('nick');  
+            
+            $table->string('rut');
+
+            $table->string('giro');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
