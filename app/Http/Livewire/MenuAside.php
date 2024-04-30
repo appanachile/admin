e813d24e8a8b6d27218a0b482391fc8275c0dcec
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-29 20:02:28 -0400

    u

---
+++ app/Http/Livewire/MenuAside.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MenuAside extends Component
+{
+    public function render()
+    {
+        return view('livewire.menu-aside');
+    }
+}
